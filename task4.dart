d0d3226316a31b1f62eb7038850c1433fa6905e7
amaal123-123 <elbaz055299@gmail.com>
2026-09-09 17:02:29 +0300

    Add files via upload

---
+++ task4.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  List<String> d=[];
+
+  d.add("saturday");
+  d.add("sunday");
+  d.add("nomday");
+  d.add("tuesday");
+  d.add("wenday");
+  d.add("thursday");
+  d.add("friday");
+  print(d);
+
+}
